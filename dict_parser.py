a0db3d6e253f02f503d7f073104642f19bda4837
Cooper Pellaton <c@cepp.at>
2016-06-30 22:14:41 -0400

    Now using NLTK.

--- dict_parser.py
+++ dict_parser.py
@@ -1,5 +1,23 @@
 import json
+import nltk
+import urllib2
 from pprint import pprint
+from nltk.corpus import *
+
+
+response = urllib2.urlopen(
+    "https://raw.githubusercontent.com/adambom/dictionary/master/dictionary.json")
+raw = response.read().decode('utf8')
+
+
+def word_links():
+    # to_analyze = nltk.data.load(, format='auto', cache=true,
+    # verbose=False, logic_parser=None, fstruct_reader=None, encoding=None)
+    tokens = nltk.word_tokenize(raw)
+    text = nltk.Text(tokens)
+    tagged = nltk.pos_tag(text)
+    entities = nltk.chunk.ne_chunk(tagged)
+    return entities
 
 
 def json_dictionary_loader():
@@ -9,4 +27,4 @@ def json_dictionary_loader():
         return d
 
 if __name__ == "__main__":
-    print "This ran correctly %s" % json_dictionary_loader()
+    print "This ran correctly %s" % word_links()
